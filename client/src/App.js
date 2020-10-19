9716db0ed149af1f1c1639db8220b898df61a517
Javier Zapata <javierzapata82@gmail.com>
2020-10-19 11:28:55 +0200

    Semantic UI & Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import { Container } from 'semantic-ui-react';
 import './App.css';
+import MenuBar from './components/MenuBar';
+import HomePage from './pages/Home';
+import LoginPage from './pages/Login';
+import RegisterPage from './pages/Register';
 
 function App() {
   return (
-    <div>
-      <h1>Hi World!</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
+      </Container>
+    </Router>
   );
 }
 
---
+++ server/src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGODB: `mongodb+srv://javi:P50lSjXxJ4L6ghbh@cluster0.tia1j.mongodb.net/merng?retryWrites=true&w=majority`,
+  SECRET_KEY: `very secret key`,
+};
